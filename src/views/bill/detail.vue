3fc3fdeced4d2fa90831a624516f0a8d3abcad18
星如雨 <liyun200104@163.com>
2024-12-14 22:27:01 +0800

    bill detail

--- src/views/bill/detail.vue
+++ src/views/bill/detail.vue
@@ -7,36 +7,67 @@ const route = useRoute()
 const formRef = ref()
 const billId = ref(Number(route.params.billId))
 const form = ref<BillVOEntity | null>()
-watch(() => route.params.billId, () => {
+
+const router = useRouter()
+const loading = ref(false)
+function handleGetBillDetail() {
+  if (loading.value)
+    return
+
+  loading.value = true
   getBill(billId.value).then(({ data }) => {
     form.value = data
+  }).finally(() => {
+    loading.value = false
   })
+}
+function handleHeaderBack() {
+  router.back()
+}
+
+watch(() => route.params.billId, () => {
+  handleGetBillDetail()
 }, {
   immediate: true,
 })
 </script>
 
 <template>
-  <el-form v-if="form" ref="formRef" :model="form" label-position="top">
-    <el-form-item label="账单标题" prop="billTitle">
-      <el-input v-model="form.billTitle" />
-    </el-form-item>
-    <el-form-item label="账单金额" prop="billAmount">
-      <el-input-number v-model="form.billAmount" :min="0" :controls="true" />
-    </el-form-item>
-    <el-form-item label="账单日期" prop="billTime">
-      <el-date-picker v-model="form.billTime" placeholder="选择日期时间" type="date" value-format="YYYY-MM-DD HH:mm:ss" />
-    </el-form-item>
-    <el-form-item label="账单类型" prop="billType">
-      <el-radio-group v-model="form.billType">
-        <el-radio value="income">
-          收入
-        </el-radio>
-        <el-radio value="spend">
-          支出
-        </el-radio>
-      </el-radio-group>
-    </el-form-item>
-  </el-form>
-  <el-empty v-else />
+  <el-container>
+    <el-header style="--el-header-height: 40px">
+      <div h-full flex="~ row" items-center>
+        <el-page-header content="账单详情" title="首页" w-full @back="handleHeaderBack" />
+      </div>
+    </el-header>
+    <el-main>
+      <el-skeleton :rows="5" :loading="loading" animated>
+        <template #template />
+        <el-form v-if="form" ref="formRef" :model="form" label-position="top">
+          <el-form-item label="账单标题" prop="billTitle">
+            <el-input v-model="form.billTitle" disabled />
+          </el-form-item>
+          <el-form-item label="账单金额" prop="billAmount">
+            <el-input-number v-model="form.billAmount" disabled :min="0" :controls="true" />
+          </el-form-item>
+          <el-form-item label="账单日期" prop="billTime">
+            <el-date-picker
+              v-model="form.billTime" placeholder="选择日期时间" type="date"
+              value-format="YYYY-MM-DD HH:mm:ss" disabled
+            />
+          </el-form-item>
+          <el-form-item label="账单类型" prop="billType">
+            <el-radio-group v-model="form.billType" disabled>
+              <el-radio value="income">
+                收入
+              </el-radio>
+              <el-radio value="spend">
+                支出
+              </el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-form>
+        <el-empty v-else />
+      </el-skeleton>
+    </el-main>
+  </el-container>
 </template>
